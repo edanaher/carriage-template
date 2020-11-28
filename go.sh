b1e36461664859d4a58ed48578b4d77a1f3f2325
Evan Danaher <github@edanaher.net>
2020-11-28 14:28:53 -0500

    Support submitting checkpoints.

--- go.sh
+++ go.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ASSIGNMENT="ForLoopPractice"
-SUBMISSION_URL="https://Project-Name.username.repl.co"
+SUBMISSION_URL="http://localhost:8080"
+
+CHECKPOINTS=false
+CHECKPOINT_VALUES="1 2 3 4"
 
 if [ $ASSIGNMENT == "AssignmentName" ]; then
   echo "Please set assignment name in go.sh"
@@ -20,11 +23,30 @@ if ! [ -f $ASSIGNMENT.java ]; then
   exit 1
 fi
 
+if [ $CHECKPOINTS == true ]; then
+  CHECKPOINT=$(cat $ASSIGNMENT.java | grep -i -o 'Checkpoint\s*=\s*\S\+' | cut -d= -f2 | xargs echo)
+  if [ -z "$CHECKPOINT" ]; then
+    echo "No checkpoint found; ensure you have a line like \"// Checkpoint = [number]\" in $ASSIGNMENT.java"
+    exit 1
+  fi
+  if [[ " $CHECKPOINT_VALUES " != *" $CHECKPOINT "* ]]; then
+    echo "Unknown checkpoint $CHECKPOINT in $ASSIGNMENT.java; should be one of $CHECKPOINT_VALUES"
+    exit 1
+  fi
+fi
+
 if cat $ASSIGNMENT.java | grep -q "ReadyForSubmission=YES"; then
-  echo "Submitting... please be patient"
+  if [ $CHECKPOINTS == true ]; then
+    echo "Submitting checkpoint $CHECKPOINT... please be patient"
+    CHECKPOINT_PARAMETER="-F checkpoint=$CHECKPOINT"
+  else
+    echo "Submitting... please be patient"
+    CHECKPOINT_PARAMETER=""
+  fi
   curl -X POST \
        -H "Content-Type: multipart/form-data" \
        -F "codefile=@$ASSIGNMENT.java" \
+       $CHECKPOINT_PARAMETER \
        $SUBMISSION_URL
 else
   javac $ASSIGNMENT.java && java $ASSIGNMENT
